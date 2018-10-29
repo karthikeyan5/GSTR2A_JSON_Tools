3aa3de20823e3ecb01c1401a0b373f04a85b1aa3
karthikeyan5 <skarthikeyan1410@gmail.com>
2018-10-29 20:49:19 +0530

    added support for b2ba invoices

--- Parse_GSTR2A.py
+++ Parse_GSTR2A.py
@@ -9,11 +9,11 @@ excel_file_name = './data/GSTR2.xlsx'
 
 
 workbook = xlsxwriter.Workbook(excel_file_name)
-worksheet_b2b = workbook.add_worksheet('b2b')
+worksheet_b2b = workbook.add_worksheet('b2b-and-b2ba')
 row = 0
 worksheet_b2b.write_row(row,0,['Filing Period','Supplier Filing Status','Supplier GSTIN','Supplier Name','Invoice Number',
                                 'Invoice Date','GSTIN of the taxpayer','Place of Supply (State Code)','Reverse Charge',
-                                'Taxable Amount','IGST Amount','CGST Amount','SGST Amount','Cess Amount','Invoice Value'])
+                                'Taxable Amount','IGST Amount','CGST Amount','SGST Amount','Cess Amount','Invoice Value','Tax Rate','Old Invoice Number','Old Invoice Date','b2ba'])
 
 def main():
     global row
@@ -27,20 +27,22 @@ def main():
                         # print(parsed_json.keys())
                         # dict_keys(['gstin', 'cdn', 'b2b', 'fp'])
                         print(parsed_json['fp'])
-                        if 'b2b' in parsed_json:
-                            for supplier in parsed_json['b2b']:
-                                for inv in supplier['inv']:
-                                    inv_values = calc_inv_value(inv['itms'])
-                                    # print(parsed_json['fp'],supplier['cfs'],supplier['ctin'],supplier['cname'],
-                                    # inv['inum'],inv['idt'],parsed_json['gstin'],parsed_json['gstin'][:2],
-                                    # inv['rchrg'],inv_values['txval'],inv_values['iamt'],inv_values['camt'],
-                                    # inv_values['samt'],inv_values['csamt'],inv['val'])
-                                    row += 1
-                                    worksheet_b2b.write_row(row,0,[parsed_json['fp'],supplier['cfs'],
-                                    supplier['ctin'],supplier['cname'],
-                                    inv['inum'],inv['idt'],parsed_json['gstin'],parsed_json['gstin'][:2],
-                                    inv['rchrg'],inv_values['txval'],inv_values['iamt'],inv_values['camt'],
-                                    inv_values['samt'],inv_values['csamt'],inv['val']])
+                        doc_type_list = ['b2b','b2ba']
+                        for doc_type in doc_type_list:
+                            if doc_type in parsed_json:
+                                for supplier in parsed_json[doc_type]:
+                                    for inv in supplier['inv']:
+                                        inv_values = calc_inv_value(inv['itms'])
+                                        if doc_type == 'b2ba':
+                                            oinum,oidt,b2ba = (inv['oinum'],inv['oidt'],'Y')
+                                        else:
+                                            oinum,oidt,b2ba = ('','','')
+                                        row += 1
+                                        worksheet_b2b.write_row(row,0,[parsed_json['fp'],supplier['cfs'],
+                                        supplier['ctin'],supplier['cname'] if 'cname' in supplier else '',
+                                        inv['inum'],inv['idt'],parsed_json['gstin'],parsed_json['gstin'][:2],
+                                        inv['rchrg'],inv_values['txval'],inv_values['iamt'],inv_values['camt'],
+                                        inv_values['samt'],inv_values['csamt'],inv['val'],','.join(str(x) for x in inv_values['tax_rate']),oinum,oidt,b2ba])
 
 
 def calc_inv_value(itms):
@@ -50,7 +52,8 @@ def calc_inv_value(itms):
         'camt':0,
         'samt':0,
         'csamt':0,
-        'val':0
+        'val':0,
+        'tax_rate':[]
     }
 
     for itm in itms:
@@ -58,6 +61,9 @@ def calc_inv_value(itms):
             if key in itm['itm_det']:
                 total[key] += itm['itm_det'][key]
                 total['val'] += itm['itm_det'][key]
+        if 'rt' in itm['itm_det']:
+            if itm['itm_det']['rt'] not in total['tax_rate']:
+                total['tax_rate'].append(itm['itm_det']['rt'])
 
     return total
 
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 convert and match GSTR2A_JSON
 
 
- - Convert GSTR2A JSON (b2b invoice only) to Excel using `Parse_GSTR2A.py`
+ - Convert GSTR2A JSON (currently b2b,b2ba invoices only) to Excel using `Parse_GSTR2A.py`
     - mention the folder containing the GSTR2A zip files in `folder_path` variable.
     - run the python file to get the excel file
